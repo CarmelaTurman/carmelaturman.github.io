8016f320d2b1fa4e883a94cabcd895718cce9667
CarmelaTurman <crmlnglnd@gmail.com>
2025-10-15 18:42:55 -0700

    created .css for recipes 1,2,3

--- recipe-minisite/scripts.js
+++ recipe-minisite/scripts.js
@@ -1,20 +1,19 @@
 // scripts.js
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Select all top-level menu items that have a submenu
+
   const menuItems = document.querySelectorAll("nav ul.menu > li");
 
   menuItems.forEach((item) => {
     const submenu = item.querySelector("ul.submenu");
     const link = item.querySelector("a");
 
-    // Only add toggle for items that have a submenu
+   
     if (submenu) {
       link.addEventListener("click", function (e) {
-        // Prevent the link from navigating
+      
         e.preventDefault();
 
-        // Close any other open submenus
         menuItems.forEach((otherItem) => {
           const otherSubmenu = otherItem.querySelector("ul.submenu");
           if (otherSubmenu && otherSubmenu !== submenu) {
@@ -22,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
           }
         });
 
-        // Toggle the clicked submenu
+      
         if (submenu.style.display === "block") {
           submenu.style.display = "none";
         } else {
@@ -32,7 +31,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
-  // Close submenus when clicking outside the menu
+
   document.addEventListener("click", function (e) {
     menuItems.forEach((item) => {
       const submenu = item.querySelector("ul.submenu");
